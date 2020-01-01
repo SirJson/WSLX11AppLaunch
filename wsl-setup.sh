11a4a63ab0b2a6c59780749d1fce17751df0dc7e
Raffael Zica <raffael@pixeltastic.de>
2020-01-01 13:02:47 +0100

    No gist pls

--- wsl-setup.sh
+++ wsl-setup.sh
@@ -2,5 +2,5 @@
 
 sudo apt install dbus-x11
 sudo dbus-uuidgen --ensure
-bash -c 'sudo wget https://gist.githubusercontent.com/SirJson/cab30feb7b7b83b087b45e0797103aa7/raw/7371c17a1dcb56ad2c9e8f0a8ca85f07dc580111/exec-x11wsl -O /usr/local/bin/exec-x11wsl && sudo chmod +x /usr/local/bin/exec-x11wsl'
+bash -c 'sudo wget https://raw.githubusercontent.com/SirJson/WSLXAppLaunch/master/exec-x11wsl -O /usr/local/bin/exec-x11wsl && sudo chmod +x /usr/local/bin/exec-x11wsl'
 echo "Ready"
