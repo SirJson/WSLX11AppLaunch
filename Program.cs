ddacd58e2dee6bde09f86d1466929802b08b6539
Raffael Zica <raffael@pixeltastic.de>
2020-02-23 02:36:14 +0100

    Tweaks

--- Program.cs
+++ Program.cs
@@ -2,18 +2,21 @@
 using System.ComponentModel;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
+using System.IO;
 
-namespace WSLXAppLaunch
+namespace RunX11App
 {
     internal class Program
     {
         [DllImport("user32.dll", SetLastError = true, CharSet= CharSet.Auto)]
         public static extern int MessageBox(int hWnd, string text, string caption, uint type);
 
-        private static void Main(string[] args) {
+        private static void Main(string[] args)
+        {
+            var appname = Path.GetFileName(Environment.GetCommandLineArgs()[0]);
             if (args.Length <= 0)
             {
-                _ = MessageBox(0, "USAGE: WSLXAppLaunch [WSLX11Application]\n\nExample: WSLXAppLaunch gedit ~/.bashrc", "Missing argument", 0);
+                _ = MessageBox(0, $"USAGE: {appname} [Command]\n\nExample: {appname} gedit ~/.bashrc", "Missing argument", 0);
                 Environment.Exit(1);
             }
 
@@ -24,16 +27,17 @@ namespace WSLXAppLaunch
                     FileName = "x410.exe",
                     Arguments = "/wm",
                     UseShellExecute = true,
-                } 
+                }
             };
+
             var appStart = new Process()
             {
                 StartInfo = new ProcessStartInfo
                 {
                     FileName = "wsl.exe",
-                    Arguments = $"exec-x11wsl {string.Join(' ',args)}",
+                    Arguments = $"-- execwsl {string.Join(' ',args)}",
                     UseShellExecute = false,
-                    CreateNoWindow = true,
+                    CreateNoWindow = false,
                     RedirectStandardError = false,
                     RedirectStandardInput = false,
                     RedirectStandardOutput = false
@@ -44,13 +48,14 @@ namespace WSLXAppLaunch
             {
                 _ = x11Srv.Start();
             }
-            catch (Exception ex) when (ex is InvalidOperationException || ex is Win32Exception || ex is PlatformNotSupportedException)
+            catch (Exception ex) when (ex is InvalidOperationException || ex is Win32Exception || ex is PlatformNotSupportedException || ex is ObjectDisposedException)
             {
-                // No X420 Server installed :(
+                _ = MessageBox(0,$"Failed to start the local X11 Server\n\n{ex.Message}\n\nStack Trace\n{ex.StackTrace}", "Failed to start X11", 0);
+                Environment.Exit(1);
             }
-            
+
             _ = appStart.Start();
-            appStart.WaitForExit();
+//appStart.WaitForExit();
         }
     }
 }
